8fbf0724dd73762da3189b5af7e54f0c8bb33de3
Aryan <aryansharma11223@gmail.com>
2020-05-19 09:17:04 -0700

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -3,17 +3,20 @@
 $(document).ready(function() {
   $("#menu").click(function() {
     $("#navbar").show(2000);
+    $("#menu").hide(2000);
   });
 });
 
 $(document).ready(function() {
   $(".close-btn").click(function() {
     $("#navbar").hide(2000);
+    $("#menu").show(2000);
   });
 });
 
 $(document).ready(function() {
   $(".nav-links").click(function() {
     $("#navbar").hide();
+    $("#menu").show();
   });
 });
