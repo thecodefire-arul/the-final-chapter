d7f18be80602f1ed87fd27f14f897fadd481f202
Aryan <aryansharma11223@gmail.com>
2020-05-19 15:20:02 -0700

    Loader Added

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
-//MEnu Button
+//Loader
+
+$(window).on("load", function(){
+  $(".loader-wrapper").fadeOut("slow");
+})
+
+
+
+//Menu Button
 
 $(document).ready(function() {
   $("#menu").click(function() {
-    $("#navbar").show(2000);
-    $("#menu").hide(2000);
+    $("#navbar").show(500);
+    $("#menu").hide(500);
   });
 });
 
 $(document).ready(function() {
   $(".close-btn").click(function() {
-    $("#navbar").hide(2000);
-    $("#menu").show(2000);
+    $("#navbar").hide(500);
+    $("#menu").show(500);
   });
 });
 
 $(document).ready(function() {
   $(".nav-links").click(function() {
-    $("#navbar").hide();
-    $("#menu").show();
+    $("#navbar").hide(500);
+    $("#menu").show(500);
   });
 });
